a51363bffea7a9657689f74cea96f93ba389a34b
FASM1 <filipeari@live.com.pt>
2024-06-15 19:08:20 +0100

    6.Make a method return a spawn point
    6.Make a method return a spawn point@38261

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -9,10 +9,14 @@ public class NewBehaviourScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Instantiate(enemyPrefab, GenerateSpawnPosition(), enemyPrefab.transform.rotation);
+    }
+    private Vector3 GenerateSpawnPosition () 
+    {
         float spawnPosX = Random.Range(-spawnRange, spawnRange);
         float spawnPosZ = Random.Range(-spawnRange, spawnRange);
         Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ);
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation);
+        return randomPos;
     }
 
     // Update is called once per frame
